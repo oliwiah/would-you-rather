cf9acaed7432aa2d000654e4175fe7aed72bf24c
Oliwia Paulus <oliwia.maria.paulus@gmail.com>
2022-01-04 00:56:26 +0100

    add header

--- would-you-rather/src/components/Header.js
+++ would-you-rather/src/components/Header.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { connect } from 'react-redux';
+import Nav from 'react-bootstrap/Nav';
 
-export const Header = () => (
-    <header>
-        <p>This is Header</p>
-    </header>
+export const Header = ({ authedUser, users }) => (
+    <Nav fill variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+            <Nav.Link href="/">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link href="/newquestion">New question</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link href="/leaderboard">Leaderboard</Nav.Link>
+        </Nav.Item>
+        {authedUser === null ? (
+            <Nav.Item>
+                <Nav.Link href="/login">Login</Nav.Link>
+            </Nav.Item>
+        ) : (
+            <Nav.Item>
+                <Nav.Link href="/logout">Logout</Nav.Link>
+            </Nav.Item>
+        )}
+    </Nav>
 );
+
+function mapStateToProps({ authedUser, users }) {
+    return {
+      authedUser,
+      users
+    };
+  }
+
+  export default connect(mapStateToProps)(Header);
