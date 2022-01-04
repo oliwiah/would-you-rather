cb4e1b25fb22e21719e1da97f941713f2f54fd39
Oliwia Paulus <oliwia.maria.paulus@gmail.com>
2022-01-04 13:13:58 +0100

    cleanup

--- would-you-rather/src/index.js
+++ would-you-rather/src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-import middleware from "./middleware";
-import reducer from "./reducers";
-import { createStore } from "redux";
-import './index.css';
-import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import middleware from './middleware';
+import reducer from './reducers';
+import App from './components/App';
+import './index.css';
 
 const store = createStore(reducer, middleware);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
