5ec3e43fe76a813968dd756941a4dad2d921f0d0
Oliwia Paulus <oliwia.maria.paulus@gmail.com>
2022-01-10 23:49:11 +0100

    switch to useSelector and add loader

--- would-you-rather/src/components/App.js
+++ would-you-rather/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { connect, useDispatch } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
+import LoadingBar from 'react-redux-loading';
 
 import { handleInitialData } from '../actions/shared';
 import Login from './Login';
@@ -11,35 +12,46 @@ import { Leaderboard } from './Leaderboard';
 import { NewQuestion } from './NewQuestion';
 import { Logout } from './Logout';
 import { NotFound } from './NotFound';
-import authedUser from '../reducers/authedUser';
 
 const App = () => {
     const dispatch = useDispatch();
+    const authedUser = useSelector((state) => state.authedUser);
+    const loading = useSelector((state) => Object.keys(state.users).length === 0);
 
     useEffect(() => {
         dispatch(handleInitialData());
     }, [dispatch]);
 
     return (
-        <Router>
-            <React.Fragment>
-                <Header />
-                {authedUser === null ? (
-                    <Login />
-                ) : (
-                    <Routes>
-                      {/* To remove login route */}
-                        <Route path="/login" element={<Login />} />
-                        <Route exact path="/" element={<Questions />} />
-                        <Route exact path="/questions/:question_id" element={<Question />} />
-                        <Route exact path="/leaderboard" element={<Leaderboard />} />
-                        <Route exact path="/newquestion" element={<NewQuestion />} />
-                        <Route exact path="/logout" element={<Logout />} />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                )}
-            </React.Fragment>
-        </Router>
+        <>
+            {loading ? (
+                <LoadingBar style={{ backgroundColor: '#5BC0DE' }} />
+            ) : (
+                <Router>
+                    <React.Fragment>
+                        <Header />
+                        {authedUser === null ? (
+                            <Login />
+                        ) : (
+                            <Routes>
+                                {/* To remove login route */}
+                                <Route path="/login" element={<Login />} />
+                                <Route exact path="/" element={<Questions />} />
+                                <Route
+                                    exact
+                                    path="/questions/:question_id"
+                                    element={<Question />}
+                                />
+                                <Route exact path="/leaderboard" element={<Leaderboard />} />
+                                <Route exact path="/newquestion" element={<NewQuestion />} />
+                                <Route exact path="/logout" element={<Logout />} />
+                                <Route path="*" element={<NotFound />} />
+                            </Routes>
+                        )}
+                    </React.Fragment>
+                </Router>
+            )}
+        </>
     );
 };
 
