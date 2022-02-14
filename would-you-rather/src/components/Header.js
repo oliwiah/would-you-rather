0419fe1f9ad6589931de7bc449ab694690de2046
Oliwia Paulus <oliwia.maria.paulus@gmail.com>
2022-02-14 01:20:34 +0100

    login and logout and navbar working properly

--- would-you-rather/src/components/Header.js
+++ would-you-rather/src/components/Header.js
@@ -1,35 +1,47 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import Nav from 'react-bootstrap/Nav';
+import { useSelector, useDispatch } from 'react-redux';
+import { Nav, Navbar, Container, NavbarBrand } from 'react-bootstrap';
+import { resetAuthedUser } from '../actions/authedUser';
 
-export const Header = ({ authedUser, users }) => (
-    <Nav fill variant="tabs">
-        <Nav.Item>
-            <Nav.Link href="/">Home</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-            <Nav.Link href="/newquestion">New question</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-            <Nav.Link href="/leaderboard">Leaderboard</Nav.Link>
-        </Nav.Item>
-        {authedUser === null ? (
-            <Nav.Item>
-                <Nav.Link href="/login">Login</Nav.Link>
-            </Nav.Item>
-        ) : (
-            <Nav.Item>
-                <Nav.Link href="/logout">Logout</Nav.Link>
-            </Nav.Item>
-        )}
-    </Nav>
-);
+export const Header = () => {
+    const dispatch = useDispatch();
+    const authedUser = useSelector((state) => state.authedUser);
+    const users = useSelector((state) => state.users);
+    const avatar = users[authedUser]?.avatarURL;
 
-function mapStateToProps({ authedUser, users }) {
-    return {
-        authedUser,
-        users,
-    };
-}
+    return (
+        <Navbar bg="light" variant="light">
+            <Container>
+            <Navbar.Brand href="/">Would you rather...?</Navbar.Brand>
+            <Nav className="me-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+            </Nav>
+            <Nav className="me-auto">
+                <Nav.Link href="/newquestion">New question</Nav.Link>
+            </Nav>
+            <Nav className="me-auto">
+                <Nav.Link href="/leaderboard">Leaderboard</Nav.Link>
+            </Nav>
+            {authedUser === null ? (
+                <Nav className="me-auto">
+                    <Nav.Link href="/login">Login</Nav.Link>
+                </Nav>
+            ) : (
+                <>
+                    <NavbarBrand>
+                        <img src={avatar} alt='Avatar' width='35'></img>
+                    </NavbarBrand>
+                    <Nav>Hello, {authedUser}!</Nav>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/logout" onClick={() => dispatch(resetAuthedUser())}>
+                            Logout
+                        </Nav.Link>
+                    </Nav>
+                </>
+            )}
+            </Container>
+        </Navbar>
+    );
+};
 
-export default connect(mapStateToProps)(Header);
+export default Header;
--- would-you-rather/src/components/Login.js
+++ would-you-rather/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { connect, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import { Form, Button } from 'react-bootstrap';
@@ -8,17 +8,17 @@ import { setAuthedUser } from '../actions/authedUser';
 const Login = (props) => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const [user, setUser] = useState('');
-    const { users, userIds } = props;
+    const users = useSelector((state) => state.users);
+    const userIds = Object.keys(users);
+    const [selectedUser, setSelectedUser] = useState(userIds[0]);
 
     const changeUser = (e) => {
-        const user = e.target.value;
-        setUser(user);
+        setSelectedUser(e.target.value);
     };
 
     const handleLogin = (e) => {
         e.preventDefault();
-        dispatch(setAuthedUser(user));
+        dispatch(setAuthedUser(selectedUser));
         navigate('/');
     };
 
@@ -29,8 +29,8 @@ const Login = (props) => {
             <Form onSubmit={handleLogin}>
                 <Form.Group className="mb-3">
                     <Form.Label htmlFor="login">Login to continue</Form.Label>
-                    <Form.Select size="lg" id="login" onChange={changeUser} value={user}>
-                        <option key="initialText" value="" disabled>
+                    <Form.Select size="lg" id="login" onChange={changeUser} value={selectedUser}>
+                        <option key="initialText" disabled>
                             Choose user
                         </option>
                         {userIds.map((userId) => (
@@ -40,7 +40,7 @@ const Login = (props) => {
                         ))}
                     </Form.Select>
                 </Form.Group>
-                <Button type="submit" disabled={user === ''}>
+                <Button type="submit" disabled={selectedUser === null}>
                     Login
                 </Button>
             </Form>
@@ -48,11 +48,4 @@ const Login = (props) => {
     );
 };
 
-function mapStateToProps({ users }) {
-    return {
-        userIds: Object.keys(users),
-        users,
-    };
-}
-
-export default connect(mapStateToProps)(Login);
+export default Login;
